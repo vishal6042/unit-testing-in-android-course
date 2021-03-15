abe7106981d6bb85812a6a1ad550a170b06c20c1
Vishal Bharti <bharti.vishal4@gmail.com>
2021-03-16 03:03:20 +0530

    exercise 2 fix

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
+
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void test1() {
+        String result = SUT.duplicate("aa");
+        Assert.assertThat(result, CoreMatchers.is("aaaa"));
+    }
+
+    @Test
+    public void test2() {
+        String result = SUT.duplicate("");
+        Assert.assertThat(result, CoreMatchers.is(""));
+    }
 
 }
