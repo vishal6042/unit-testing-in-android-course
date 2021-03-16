3fa0585d27488b8d0acb62cb08f4ae72262193cf
Vishal Bharti <bharti.vishal4@gmail.com>
2021-03-17 02:28:26 +0530

    exercise 3 fix

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,82 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class IntervalsAdjacencyDetectorTest {
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+    @Test
+    public void test1() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(8, 12);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test2() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(5, 12);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(true));
+    }
+    @Test
+    public void test3() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(4, 12);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test4() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(-1, 5);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test5() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(0, 2);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test6() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(-8, 12);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test7() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(-8, -5);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+    @Test
+    public void test8() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(-8, -1);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(true));
+    }
+    @Test
+    public void test9() {
+        Interval interval1 = new Interval(-1, 5);
+        Interval interval2 = new Interval(-8, 2);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
 
 }
